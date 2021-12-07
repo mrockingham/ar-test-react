8f098ef92e59998d859cce79608bdd93b5f371ec
Michael Rockingham <mrockingham2@gmail.com>
2021-12-07 18:30:17 -0500

    change stuff

--- arreact/src/components/ArViewer.js
+++ arreact/src/components/ArViewer.js
@@ -2,13 +2,6 @@ import useScript from '../hooks/useScript';
 // import 'aframe';
 
 export const ArViewer = () => {
-  // useScript('https://aframe.io/releases/1.0.4/aframe.min.js');
-  // useScript(
-  //   'https://unpkg.com/aframe-look-at-component@0.8.0/dist/aframe-look-at-component.min.js'
-  // );
-  // useScript(
-  //   'https://raw.githack.com/AR-js-org/AR.js/master/aframe/build/aframe-ar-nft.js'
-  // );
   return (
     <div style={{ margin: '0', overflow: 'hidden' }}>
       <a-scene
@@ -20,8 +13,9 @@ export const ArViewer = () => {
           value="Mike is here."
           scale="0.1 0.1 0.1"
           gps-entity-place="latitude: 29.1724435; longitude: -81.0011331;"
+          animation-mixer
         ></a-text>
-        <a-camera gps-camera></a-camera>
+        <a-camera gps-camera rotation-reader></a-camera>
       </a-scene>
     </div>
   );
