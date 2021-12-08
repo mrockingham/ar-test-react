ff9c00ae5cf2d3c1d0bc04b8806445c79d3b0517
Michael Rockingham <mrockingham2@gmail.com>
2021-12-07 20:38:30 -0500

    try react with box

--- arreact/src/components/ArViewer.js
+++ arreact/src/components/ArViewer.js
@@ -7,14 +7,20 @@ export const ArViewer = () => {
       test
       <a-scene
         vr-mode-ui="enabled: false"
-        embedded
-        arjs="sourceType: webcam;  debugUIEnabled: false;"
+        arjs="sourceType: webcam; videoTexture: true; debugUIEnabled: false;"
+        renderer="antialias: true; alpha: true"
       >
         <a-text
           value="Mike is here."
-          scale="50 50 50"
-          gps-entity-place="latitude: 29.1724301; longitude: -81.0010849;"
+          scale="10 10 10"
+          gps-entity-place="latitude: 29.1724435; longitude: -81.0011331;"
+          // gps-entity-place="latitude: 29.1724301; longitude: -81.0010849;"
         ></a-text>
+        <a-box
+          gps-projected-entity-place="latitude: 29.1724435; longitude: -81.0011331;"
+          material="color: red"
+          scale="10 10 10"
+        ></a-box>
         <a-camera gps-camera rotation-reader></a-camera>
       </a-scene>
     </div>
