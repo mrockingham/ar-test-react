cd0129ae3582b7b0fa9db13c88c285f4687e8926
Michael Rockingham <mrockingham2@gmail.com>
2021-12-07 21:51:21 -0500

    sim

--- arreact/src/components/ArViewer.js
+++ arreact/src/components/ArViewer.js
@@ -7,25 +7,22 @@ export const ArViewer = () => {
       test
       <a-scene
         vr-mode-ui="enabled: false"
-        embedded
+        // embedded
         arjs="sourceType: webcam; videoTexture: true; debugUIEnabled: false;"
-        renderer="antialias: true; alpha: true"
+        // renderer="antialias: true; alpha: true"
       >
         <a-text
           value="Mike is here."
           scale="10 10 10"
-          gps-entity-place="latitude: 29.1724435; longitude: -81.0011331;"
+          gps-entity-place="latitude: 29.1724672; longitude: -81.0011695;"
           // gps-entity-place="latitude: 29.1724301; longitude: -81.0010849;"
         ></a-text>
-        <a-box
+        {/* <a-box
           gps-projected-entity-place="latitude: 29.1724435; longitude: -81.0011331;"
           material="color: red"
           scale="10 10 10"
-        ></a-box>
-        <a-camera
-          gps-camera="gpsMinDistance: 20; simulateLatitude: 29.1724435; simulateLongitude: -81.0011331;"
-          rotation-reader
-        ></a-camera>
+        ></a-box> */}
+        <a-camera gps-camera rotation-reader></a-camera>
       </a-scene>
     </div>
   );
